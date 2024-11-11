0b0d08be152a6f3da25503726706c841154d0e90
Boya Jayanthi <boyajayanthi100@gmail.com>
2024-11-11 16:10:31 +0530

    based on earthquake magnitude - location colours have been changed

--- src/components/EarthquakeMarker/index.js
+++ src/components/EarthquakeMarker/index.js
@@ -3,19 +3,45 @@ import React from 'react';
 import { Marker, Popup, Tooltip } from 'react-leaflet';
 import L from 'leaflet';
 
-const earthquakeIcon = 
-new L.Icon({
-  iconUrl: './location-dot-solid.svg',
-  iconSize: [32, 32],  
-  iconAnchor: [16, 32],  
+
+// Define the icons for different magnitudes
+const weakIcon = new L.Icon({
+  iconUrl: './location-dot-green-solid.svg',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
   popupAnchor: [0, -32],
 });
+
+const moderateIcon = new L.Icon({
+  iconUrl: './location-dot-yellow-solid.svg',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
+const strongIcon = new L.Icon({
+  iconUrl: './location-dot-solid.svg',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
 
 
 const EarthquakeMarker = ({ feature }) => {
   const { coordinates } = feature.geometry;
   const { mag, place, url, status, time } = feature.properties;
 
+
+  // Determine the appropriate icon based on magnitude
+  let earthquakeIcon;
+  if (mag < 3) {
+    earthquakeIcon = weakIcon; // Weak (Magnitude < 3)
+  } else if (mag >= 3 && mag < 5) {
+    earthquakeIcon = moderateIcon; // Moderate (3 <= Magnitude < 5)
+  } else {
+    earthquakeIcon = strongIcon; // Strong (Magnitude >= 5)
+  }
 
   // Convert Unix timestamp to Date object
   const timestamp = new Date(time);
